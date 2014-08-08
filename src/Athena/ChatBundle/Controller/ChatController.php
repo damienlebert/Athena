2e31f513ca9a3912c771b03d46306c70a2bf6fe4
Pierre <Pierre>
2014-08-08 16:34:19 +0200

    Initialisation utilisateurs

--- src/Athena/ChatBundle/Controller/ChatController.php
+++ src/Athena/ChatBundle/Controller/ChatController.php
@@ -3,11 +3,24 @@
 namespace Athena\ChatBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Athena\UserBundle\Entity\UserRepository;
 
 class ChatController extends Controller
 {
     public function welcomeAction()
     {
         return $this->render('AthenaChatBundle:Pages:index.html.twig');
+    }
+    
+    
+    public function usersAction()
+    {
+    	
+    	$liste_users = $this->getDoctrine()
+                         ->getManager()
+                         ->getRepository('AthenaUserBundle:User')
+                         ->findAll();
+    	
+    	return $this->render('AthenaChatBundle:Pages:users.html.twig', array('msg' => $liste_users));
     }
 }
--- src/Athena/UserBundle/Entity/UserRepository.php
+++ src/Athena/UserBundle/Entity/UserRepository.php
@@ -12,4 +12,11 @@ use Doctrine\ORM\EntityRepository;
  */
 class UserRepository extends EntityRepository
 {
+	
+
+	
+	
+	
+	
+	
 }
