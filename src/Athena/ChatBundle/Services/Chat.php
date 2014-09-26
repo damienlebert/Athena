f42588ee9ab7c1f83f8a268d826cd158b1d19999
PierreVassoilles <pierre.vassoilles@gmail.com>
2014-09-26 16:01:37 +0200

    implémentation méthodes : - disableConversation - enableConversation - addMessage
    Création méthode :
    - modifyConversationStatus

--- src/Athena/ChatBundle/Services/Chat.php
+++ src/Athena/ChatBundle/Services/Chat.php
@@ -2,6 +2,9 @@
 namespace Athena\ChatBundle\Services;
 
 use Doctrine\ORM\EntityManager;
+use Athena\ChatBundle\Entity\LinkUsrConversation;
+use Athena\ChatBundle\Entity\Message;
+use Athena\UserBundle\Entity\User;
 
 /**
  * Service Chat
@@ -40,7 +43,16 @@ class Chat
 	 */
 	public function findUsers($keyword)
 	{
-	    
+	    $liste_users = $this->em->getRepository('AthenaUserBundle:User')
+	                   ->createQueryBuilder('u')
+                       ->where('u.username like :keyword')
+                       ->orWhere('u.firstName like :keyword')
+                       ->orWhere('u.lastName like :keyword')
+                       ->setParameter('keyword', '%'.$keyword.'%')
+                       ->getQuery()
+                       ->getResult();
+	     
+	    return $liste_users;
 	}
 	
 	/**
@@ -84,30 +96,80 @@ class Chat
 	
 	/**
 	 * Désactive la conversation $id
-	 * @param integer $id
+	 * @param integer $idConversation
+	 * @param integer $idUser
 	 */
-	public function disableConversation($id)
+	public function disableConversation($idConversation, $idUser)
 	{
-		
+	    $this->modifyConversationStatus($idConversation, $idUser, 0);
 	}
 	
 	/**
 	 * Active la conversation $id
-	 * @param integer $id
+	 * @param integer $idConversation
+	 * @param integer $idUser
 	 */
-	public function enableConversation($id)
+	public function enableConversation($idConversation, $idUser)
 	{
-		
+	    $this->modifyConversationStatus($idConversation, $idUser, 1);
 	}
 	
 	/**
-	 * Ajoute un message à la conversation $idConversation
+	 * Modifie le statut d'une conversation
+	 * 1 =  ouverte
+	 * 0 = fermée
 	 * @param integer $idConversation
+	 * @param integer $idUser
+	 * @param integer $statut
+	 * @throws \InvalidArgumentException
+	 */
+	public function modifyConversationStatus($idConversation, $idUser, $statut)
+	{
+	    if(0 === (int) $idConversation){
+	        throw new \InvalidArgumentException("La conversation n'existe pas.");
+	    }
+	    
+	    if(0 === (int) $idUser){
+	        throw new \InvalidArgumentException("L'utilisateur n'existe pas.");
+	    }
+	    
+	    if($statut != 0 && $statut != 1){
+	        throw new \InvalidArgumentException("Le statut de la conversation doit être 0 ou 1.");
+	    }
+	    
+	    //LinkUsrConversation
+	    $linkUsrConv = $this->em
+                    	    ->getRepository('AthenaChatBundle:LinkUsrConversation')
+                    	    ->find(array('conversation' => $idConversation, "user" => $idUser));
+	     
+	    
+	    
+	    $linkUsrConv->setStatut($statut);
+	    
+	    $this->em->persist($linkUsrConv);
+	    $this->em->flush();
+	}
+	
+	/**
+	 * Ajoute un message à la conversation $idConversation, date courante et utilisateur connecté
+	 * @param integer $idConversation
+	 * @param User $user
 	 * @param string $content
 	 */
-	public function addMessage($idConversation, $content)
+	public function addMessage($idConversation, User $user, $content)
 	{
+	    if(0 === (int) $idConversation){
+	        throw new \InvalidArgumentException("La conversation n'existe pas.");
+	    }
+	    
+		$message = new Message();
+		$message->setContenu($content)
+		        ->setConversation($idConversation)
+		        ->setId_user($user)
+		        ->setDate(new \DateTime("now"));
 		
+		$this->em->persist($message);
+		$this->em->flush();
 	}
 	
 	/**
